529f51ab3bd9dc4d000f68b0fe6abadd84352c98
yournatalita <egorova@aeroidea.ru>
2020-09-28 21:30:16 +0300

    Test Variables Deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-// TODO: remove debugging
-console.log(process.env);
-
-
 const path = require('path');
 
-require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 const nodeEnv = process.env.NODE_ENV;
 const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
@@ -14,6 +10,9 @@ const withImages = require('next-images');
 
 const baseUrl = process.env.API_URL;
 const siteUrl = process.env.SITE_URL || process.env.VERCEL_URL;
+
+// TODO: remove debugging
+console.log(process.env);
 
 
 const config = {
